5737f7e4a092485d982c0af9e9a1964f8cc48be5
Sketch6ick <93180779+Sketch6ick@users.noreply.github.com>
2024-12-22 23:13:43 +0700

    first commit

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// src/reportWebVitals.js
+// import { reportWebVitals } from 'web-vitals'; // ลบการ import จาก web-vitals ถ้าไม่ใช้
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
